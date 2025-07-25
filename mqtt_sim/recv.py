7a10521a432db60f5e73c1de58aed63d78944e26
alexandrosSamartzis <alexandros.samartzisde@gmail.com>
2025-07-25 16:09:14 +0200

    add logs in recv fiel

--- mqtt_sim/recv.py
+++ mqtt_sim/recv.py
@@ -2,11 +2,27 @@
 
 import paho.mqtt.client as mqtt
 import json
+import logging
+from datetime import datetime
 
 # --- MQTT Config ---
 BROKER = "mqtt-broker"  # docker-compose service name
 PORT = 1883
 TOPIC_FILTER = "machine/+/metrics"  # '+' matches any machine_id
+
+# === Logging Setup ===
+logging.basicConfig(
+    filename='logs/recv_messages.log',
+    filemode='a',  # Append mode
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+# === Print in screen ===
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
 # --- Callback on connection ---
 def on_connect(client, userdata, flags, rc):
@@ -19,6 +35,7 @@ def on_message(client, userdata, msg):
     topic = msg.topic
     #print(f"Received from {msg.topic}: {payload}")
     if payload["maintenance_required"] == 1:
+        logging.info(f"Received message from topic {msg.topic}: {payload}")
         print(topic)
         print("⚠️⚠️⚠️⚠️  Maintenance needed for Machine", payload["machine_id"])
 
