f95ed4c9922d5eeb2a02ff8cec074a4729178f38
alexandrosSamartzis <alexandros.samartzisde@gmail.com>
2025-07-25 06:48:20 +0200

    small change in pytests

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import os
 
+# test the file path of the data file, the data qualification and completion of the data
 def test_csv_load():
-    file_path = "../../data/smart_manufacturing_data.csv"
+    file_path = "./data/smart_manufacturing_data.csv"
     assert os.path.exists(file_path), "CSV file missing!"
     df = pd.read_csv(file_path)
     assert not df.empty, "Dataframe is empty"
     assert "temperature" in df.columns, "Missing expected column"
 
+# simple check test of logic in the code
 def test_sample_logic():
     assert 1 + 1 == 2
+    
